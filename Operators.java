87f7c18a227bd396d31be10ed907e1f82b444d6a
KENAYI <chikenayiweli@gmail.com>
2024-08-08 16:15:40 +0100

    Operators

---
+++ Operators.java
@@ -0,0 +1,47 @@
+class Operators {
+    public static void main(String[] args) {
+        int num1 =12;
+        int num2 =15;
+        int age = 21;
+        int score = 0;
+        int turns = 10;
+        int num3 = 55;
+
+        boolean isStudent = false;
+        boolean isLibMember = true;
+
+        //num1 += 5;
+        //num1 %= 2;
+
+        score++;
+        turns--;
+
+        //is equal to 
+        //System.out.println(num1 == num2);
+
+        //is not equal to
+        //System.out.println(num1 != num2);
+
+        //num increments by 5
+        //System.out.println(num1);
+
+        //and operator for int
+        //System.out.println(age >=18 && age <=40);
+
+        //or operator for boolean
+        //System.out.println(isStudent || isLibMember);
+
+        //not operator for boolean
+        //System.out.println(!isStudent);
+
+        //increments and decrements
+        //System.out.println(score);
+        //System.out.println(turns);
+
+        // increments for int
+        //System.out.println(num3++);
+        //System.out.println(num3);
+        // OR simply:
+        System.out.println(++num3);
+    }        
+}
